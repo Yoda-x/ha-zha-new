3034ddadf9924a1089552c7781ffeff47cbffbb8
Yoda-x <Yoda-x>
2018-09-06 09:13:51 +0200

    light multicast, use all groups and not only the {0]

--- custom_components/light/zha_new.py
+++ custom_components/light/zha_new.py
@@ -186,6 +186,7 @@ class Light(zha_new.Entity, light.Light):
 
         if self._groups:
             result = await self._endpoint.groups.get_membership([])
+            _LOGGER.debug("%s get membership: %s", self.entity_id,  result)
             if result:
                 if result[0] >= 1:
                     self._groups = result[1]
