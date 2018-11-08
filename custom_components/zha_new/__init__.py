759848c181a805885d3edc9a461756325f1f7a49
Yoda-x <34125458+Yoda-x@users.noreply.github.com>
2018-11-08 14:13:20 +0100

    update bellows to 0.7.4.1

--- custom_components/zha_new/__init__.py
+++ custom_components/zha_new/__init__.py
@@ -19,7 +19,7 @@ from homeassistant.helpers import discovery, entity
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.util import slugify
 from importlib import import_module
-REQUIREMENTS = ['https://github.com/Yoda-x/bellows/archive/master.zip#bellows==0.7.4', 
+REQUIREMENTS = ['https://github.com/Yoda-x/bellows/archive/master.zip#bellows==0.7.4.1', 
                 'https://github.com/Yoda-x/zigpy/archive/master.zip#zigpy==0.1.4-Y']
 DOMAIN = 'zha_new'
 
